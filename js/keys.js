4b895e859672335796a042fa4fe9b5b090c7a4a0
benjapg <benjamin.paul.graham@okstate.edu>
2016-03-12 20:33:25 -0600

    Create keys.js

---
+++ js/keys.js
@@ -0,0 +1,46 @@
+//rename this file to keys.js after you fill in the relevant information to enable functionality
+
+var keys = {
+    weather: {
+		apiKey: 'fdd7e63da5bac9392c057a4fc7db4bd1',
+		//Go to https://developer.forecast.io/ to register and get your free API key
+		address: 'Rockwall, TX'
+    },
+	traffic: {
+		params: {
+			origin: 'place_id:ChIJUctVeX-qToYR_TRap--d99Q',
+			destination: 'place_id:ChIJ7-fWuITnS4YRJG8fRn50h9Q',
+            // Use the PlaceID Finder: https://developers.google.com/maps/documentation/javascript/examples/places-placeid-finder
+			departure_time: 'now',
+			key: 'AIzaSyDHILVSmsM8NEqOdyd9I9XoppPp3jkf0Do'
+            // Go here to get your Google Maps API key: https://developers.google.com/maps/documentation/directions/
+		}
+	},
+	//calendar: {
+		//maximumEntries: 10, // Total Maximum Entries
+		//displaySymbol: true,
+		//defaultSymbol: 'calendar', // Fontawsome Symbol see http://fontawesome.io/cheatsheet/
+		//urls: [
+			{
+				//symbol: 'calendar-o',
+				//url: 'CALENDAR.ics ADDRESS'
+				// For Google Calendar, it's the 'Private Address' under Calendar Settings when viewing your Calendar
+			},
+			{
+				//symbol: 'calendar-check-o',
+				//url: 'CALENDAR.ics ADDRESS'
+			}
+		]
+	},
+	birthdays: [
+		{
+			day:14,
+			month:March,
+			name:'Holly'
+		},{
+			day:15,
+			month:January,
+			name:'Ben'
+		}
+	]
+}
